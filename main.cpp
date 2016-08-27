f3acdbc4f2cc6cbf1f1967860132977b49543699
nilspin <gooney28@gmail.com>
2016-08-27 06:47:22 +0530

    added simple sphere

--- main.cpp
+++ main.cpp
@@ -7,8 +7,23 @@
 using namespace std;
 using namespace glm;
 
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f*dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+
+    float discrimiant = (b*b - 4*a*c);
+    return (discrimiant > 0);
+}
+
 vec3 color(const ray& r)
 {
+    if(hit_sphere(vec3(0.0f, 0.0f, -2.0f), 0.5f, r))
+    {
+        return vec3(1.0f, 0.0f, 0.0f);
+    }
     vec3 unit_dir = normalize(vec3(r.direction()));
     float t = 0.5f*(unit_dir.y + 1.0f);
     //return blend between blue and white
