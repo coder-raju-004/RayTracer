fa6979232957b2ec005f053c1aaa38cb61e5c44c
nilspin <gooney28@gmail.com>
2016-08-27 05:39:28 +0530

    fix typo

--- ray.hpp
+++ ray.hpp
@@ -5,17 +5,17 @@
 
 using namespace glm;
 
-class ray(){
+class ray{
     public:
         ray() {}
         ray(const vec3& a, const vec3& b){A = a; B = b;}
         vec3 origin() const     {return A;}
-        vec3 direction() const  {return Bl}
+        vec3 direction() const  {return B;}
         vec3 point_at_parameter(float t) const {return A + B*t;}
 
     private:
         vec3 A;
         vec3 B;
-}
+};
 
 #endif
