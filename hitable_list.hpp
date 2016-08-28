801d031ae7ffb976b273f4063ef2b737c961b068
nilspin <gooney28@gmail.com>
2016-08-28 17:40:08 +0530

    fix typo in hitable_list

--- hitable_list.hpp
+++ hitable_list.hpp
@@ -17,8 +17,8 @@ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) 
 {
     hit_record temp_rec;
     bool hit_anything = false;
-    double closest_so_far = t_max;
-    for(int i = 0, i < list_size; ++i)
+    float closest_so_far = t_max;
+    for(int i = 0; i < list_size; ++i)
     {
         if(list[i]->hit(r, t_min, t_max, temp_rec))
         {
