23d6a25d00d08ff92b4b6f2c2a69807689130eea
nilspin <gooney28@gmail.com>
2016-08-28 19:03:00 +0530

    add camera class, clean up main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<limits>
 #include<fstream>
+#include<random>
 #include<glm/glm.hpp>
 #include "ray.hpp"
 #include "hitable_list.hpp"
 #include "sphere.hpp"
+#include "camera.h"
 
 using namespace std;
 using namespace glm;
@@ -25,13 +27,16 @@ vec3 color(const ray& r, hitable *world)
         return (1.0f-t)*vec3(1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
     }
 }
+
 int main()
 {
     ofstream fout;
     fout.open("picture.ppm");
 
-    int nx=300;
-    int ny=150;
+    int nx=300;     //x-resolution
+    int ny=150;     //y-resolution
+    int ns = 100;   //number of samples per pixel
+
     fout<<"P3\n"<< nx << " " << ny << "\n255\n";
     vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
     vec3 horizontal(4.0f, 0.0f, 0.0f);
@@ -44,16 +49,22 @@ int main()
 
     hitable *world = new hitable_list(list, 2);
 
+    Camera cam;
     for(int j=ny-1; j>=0; j--)
     {
         for(int i=0;i < nx; ++i)
         {
-            float u = float(i)/float(nx);
-            float v = float(j)/float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = vec3(0.0f, 0.0f, 0.0f);
+            for(int sample = 0; sample < ns; ++sample)
+            {
+                float u = float(i + drand48())/float(nx);
+                float v = float(j + drand48())/float(ny);
+                ray r=cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
 
-            vec3 p = r.point_at_parameter(1.0); //why 2?
-            vec3 col = color(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99*col.r);
             int ig = int(255.99*col.g);
             int ib = int(255.99*col.b);
