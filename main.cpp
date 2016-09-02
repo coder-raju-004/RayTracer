76e2ead5a4f306b4923cfeab359381f4affbd613
nilspin <gooney28@gmail.com>
2016-09-02 15:47:00 +0530

    add different method for generating rays for secondary light bounces

--- main.cpp
+++ main.cpp
@@ -15,11 +15,13 @@ using namespace glm;
 vec3 random_point_in_unit_sphere()
 {
     vec3 p;
-    do
-    {
-        p = 2.0f*vec3(drand48(), drand48(), drand48()) - vec3(1.0f);
-    }
-    while(length2(p) >= 1.0f);
+    p = 2.0f*(vec3(drand48(), drand48(), drand48())) - vec3(1.0f);
+//    do
+//    {
+//        p = 2.0f*vec3(drand48(), drand48(), drand48()) - vec3(1.0f);
+//    }
+//    while(length2(p) >= 1.0f);
+//    cout<<length(p)<<" ";
     return p;
 }
 
